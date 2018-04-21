c82b3a8a6e82d6aa2d65bed61a40af6990376448
lalogaribo <lalo.garibo90@gmail.com>
2018-04-21 17:56:38 -0500

    Created CRUD for flights and added admin field to users

--- app/controllers/vuelos_controller.rb
+++ app/controllers/vuelos_controller.rb
@@ -1,9 +1,54 @@
 class VuelosController < ApplicationController
+  before_action :set_vuelo, only: [:show, :edit, :update, :destroy]
   def index
-    @vuelos = current_user.vuelos
+    if logged_in? && current_user.admin?
+      @vuelos = Vuelo.all
+    else
+      @vuelos = current_user.vuelos
+      end
   end
 
   def new
     @vuelo = Vuelo.new
+  end
+
+  def create
+    @vuelo = Vuelo.new(vuelo_params)
+    if @vuelo.save
+      flash[:success] = 'Vuelo dado de alta exitosamente'
+      redirect_to vuelos_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+  def show
+  end
+
+  def update
+    if @vuelo.update(vuelo_params)
+      flash[:success] = 'Vuelo editado correctamente'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @vuelo.destroy
+      flash[:success] = 'Vuelo eliminado exitosamente'
+      redirect_to vuelos_path
+    end
+  end
+
+  private
+
+  def vuelo_params
+    params.require(:vuelo).permit(:user_id, :predio, :aplicacion, :piloto, :precio_vuelo)
+  end
+
+  def set_vuelo
+    @vuelo = Vuelo.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418003829) do
+ActiveRecord::Schema.define(version: 20180421215830) do
 
   create_table "info_predio_detalles", force: :cascade do |t|
     t.integer "material_id"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20180418003829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "vuelos", force: :cascade do |t|
