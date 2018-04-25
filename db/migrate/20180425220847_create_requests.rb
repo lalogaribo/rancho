bec8e36ba62853118e41c665d4542526711d27ab
lalogaribo <lalo.garibo90@gmail.com>
2018-04-25 17:20:08 -0500

    Removed solicitud_vuelos table

--- db/migrate/20180425220847_create_requests.rb
+++ db/migrate/20180425220847_create_requests.rb
@@ -2,7 +2,7 @@ class CreateRequests < ActiveRecord::Migration[5.1]
   def change
     create_table :requests do |t|
       t.references :user, foreign_key: true
-
+      t.string :predio
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424130708) do
+ActiveRecord::Schema.define(version: 20180425221812) do
 
   create_table "info_predio_detalles", force: :cascade do |t|
     t.integer "material_id"
@@ -84,6 +84,14 @@ ActiveRecord::Schema.define(version: 20180424130708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "predio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
