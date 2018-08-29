9f7a6e29958ec0ddead953fcae338cfac3e7e4d5
lalogaribo <lalo.garibo90@gmail.com>
2018-08-28 23:34:17 -0500

    Move mailers methods from ApplicationMailer to UserMailer and fixed small issue on the to: property

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,16 @@
 class UserMailer < ApplicationMailer
+  def registration_confirmation(user)
+    @user = user
+    mail(to: "#{user.name} <#{user.email}>", subject: 'Confirmacion de Registro')
+  end
+
+  def password_reset(user)
+    @user = user
+    mail(to: "#{user.name} <#{user.email}>", subject: 'Cambio de Contraseña')
+  end
+
+  def send_signup_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Thanks for signing up for our amazing app')
+  end
 end
