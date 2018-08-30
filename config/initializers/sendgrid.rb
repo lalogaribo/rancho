7e28f45555649f9b44593906b90e19aa11bc71a8
lalogaribo <lalo.garibo90@gmail.com>
2018-08-29 20:25:24 -0500

    Add env variable for sendgrid apikey

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,8 +1,9 @@
 ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey',
-    :password => Rails.application.secrets.SENDGRID_API_KEY,
+    :password => Rails.application.secrets.sendgrid_api_key,
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
 }
+
