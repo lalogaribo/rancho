1e5e6512e1593a9864e47e4ac1c7c7fde8d29447
Isanchez <isanchez@tiempodevelopment.com>
2018-08-30 23:33:21 -0500

    Fix worker Module

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -2,7 +2,7 @@ class Worker < ApplicationRecord
   validates :name, presence: {message: "Nombre es requerido"}
   validates :last_name, presence: {message: "Apellido es requerido"}
   validates :phone_number, presence: {message: "Telefono es requerido"},
-            length: {maximum: 10},
+            length: {maximum: 10, message: 'Ingresar un numero valido'},
             numericality: {message: 'Ingresar numero valido'},
             format: {with: /\A\d+(?:\.\d{0,2})?\z/, message: 'Ingresar numero valido'}
   belongs_to :user
