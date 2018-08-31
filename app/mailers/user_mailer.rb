136e782bdffae60bf190839c0fe12cbc2c290de7
Isanchez <isanchez@tiempodevelopment.com>
2018-08-31 18:36:04 -0500

    Merge branch 'master'

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,21 @@
 class UserMailer < ApplicationMailer
+  def registration_confirmation(user)
+    @user = user
+    mail(to: "#{user.name} <#{user.email}>", subject: 'Confirmacion de Registro')
+  end
+
+  def password_reset(user)
+    @user = user
+    mail(to: "#{user.name} <#{user.email}>", subject: 'Cambio de Contraseña')
+  end
+
+  def reset_chart_token(user)
+    @user = user
+    mail to: "#{user.name} <#{user.email}", subject: 'Nuevo token para generacion de graficas'
+  end
+
+  def send_signup_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Thanks for signing up for our amazing app')
+  end
 end
